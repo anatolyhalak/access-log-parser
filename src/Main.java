eaa473853bf90c51afd63e34fcd52d841a20ddf7
Anatoly Khalak <inno0747@gmail.com>
2022-11-27 19:36:57 +0300

    Ввод чисел и вывод в консоль результаты их вычислений

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        double sum = (double) firstNumber + secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+
+        double difference = (double) firstNumber - secondNumber;
+        System.out.println("Разность чисел: " + difference);
+
+        double productOfNumbers = (double) firstNumber * secondNumber;
+        System.out.println("Произведение чисел: " + productOfNumbers);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное чисел: " + quotient);
     }
 }
