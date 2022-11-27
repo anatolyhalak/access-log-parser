0b58e910b3b718f827c79aa509ad868db5db68d8
Anatoly Khalak <inno0747@gmail.com>
2022-11-27 15:31:00 +0300

    вывод длины текста, создание jar файла

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите текст и нажмите <Enter>: ");
+        String text = new Scanner(System.in) .nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
