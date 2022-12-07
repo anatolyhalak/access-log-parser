2061bf81161863c5a3cf121744a2811b5bda08c1
Anatoly Khalak <inno0747@gmail.com>
2022-12-08 00:53:56 +0300

    Задание по теме "Циклы"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int count = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean isExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!isExists || isDirectory) {
+                System.out.println("Файл не существует или указаный путь является путем к папке, а не к файлу");
+                continue;
+            }
+            if (isExists) {
+                count++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count);
+            }
+        }
     }
 }
